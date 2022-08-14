aff6902463f9ec9e22660de51423b12a37c49cf1
Sagitari01 <michael.catalan.tejo@gmail.com>
2022-08-13 23:40:36 -0400

    Se agrega conexion a base de datos

--- Ferme/settings.py
+++ Ferme/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'src'
 ]
 
 MIDDLEWARE = [
@@ -75,9 +76,11 @@ WSGI_APPLICATION = 'Ferme.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'mdy202201_high',
+        'USER': 'FERME',
+        'PASSWORD': 'T32^!BfUq28e',
+    },
 }
 
 
