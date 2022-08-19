b32f0f8cc604f9039969eb4d383b2d6954ee59d0
Jean G MAC <jean.gonzalez@gmail.com>
2022-08-18 20:54:25 -0400

    creando vista productos

--- Ferme/settings.py
+++ Ferme/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'src'
+    'src',
+    'widget_tweaks',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- Ferme/urls.py
+++ Ferme/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
 from src import views, static
 from src.views import Index, Login, addProducts
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
