89310e720663c49211ae818b233a3b770c802aff
LIsseteDiaz <mireen.d.e@gmail.com>
2022-08-30 00:27:30 -0400

    Se arregla el ingreso de usuarios y se crea el cerrar sesion

--- Ferme/urls.py
+++ Ferme/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from src import views, static 
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('index/', views.Index, name="index"),
     path('ingreso_usuarios/', views.Ingreso, name="ingreso"),
--- src/forms.py
+++ src/forms.py
@@ -1,5 +1,13 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Producto, Persona, Direccion, Usuario, Cliente
+
+class FormRegistroUsuario(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ("email","password1","password2")
 
 class FormClienteNormal1(forms.ModelForm):
     
